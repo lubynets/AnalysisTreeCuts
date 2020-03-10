2450f858d336c32cc6434600c2042718a1ab148f
Viktor Klochkov <v.klochkov@gsi.de>
2020-03-10 11:14:17 +0100

    Update CbmCuts.h

--- CbmCuts.h
+++ CbmCuts.h
@@ -2,19 +2,56 @@
 #include "AnalysisTree/SimpleCut.h"
 
 AnalysisTree::Cuts* GetCbmEventCuts() {
-  AnalysisTree::SimpleCut vtx_x("vtx_x", -10, 10);
-  AnalysisTree::SimpleCut vtx_y("vtx_y", -10, 10);
-  AnalysisTree::SimpleCut vtx_z("vtx_z", -10, 10);
-  AnalysisTree::SimpleCut vtx_chi2("vtx_chi2", 0.5, 3);
-  AnalysisTree::Cuts* event_cuts{ new AnalysisTree::Cuts("RecEventHeader")};
-  event_cuts->AddCuts({vtx_x, vtx_y, vtx_z, vtx_chi2});
+  Cuts* event_cuts = new Cuts("event_cuts");
+  SimpleCut vtx_x_cut("vtx_x", -0.5, 0.5);
+  SimpleCut vtx_y_cut("vtx_y", -0.5, 0.5);
+  SimpleCut vtx_z_cut("vtx_z", -0.02, 0.02);
+  SimpleCut vtx_chi2_cut("vtx_chi2", 0.5, 3);
+  event_cuts->AddCuts( {vtx_x_cut, vtx_y_cut, vtx_z_cut, vtx_chi2_cut} );
   return event_cuts;
 }
 
 AnalysisTree::Cuts* GetCbmTrackCuts() {
-//   AnalysisTree::SimpleCut vtx_chi2("vtx_chi2", -1000, 18.6);
-//   AnalysisTree::Cuts* rec_track_cuts{ new AnalysisTree::Cuts("VtxTracks")};
-  AnalysisTree::Cuts* rec_track_cuts{nullptr};
-//   rec_track_cuts->AddCut(vtx_chi2);
-  return rec_track_cuts;
+  Cuts* vertex_tracks_cuts = new Cuts("VertexTracks");
+  SimpleCut vtx_chi2_track_cut("vtx_chi2", 0, 3);
+  SimpleCut nhits_cut("nhits", 4, 100);
+  SimpleCut chi2_cut({"chi2", "nhits"}, [](std::vector<double> par){ return par[0]/par[1] < 10; });
+
+  vertex_tracks_cuts->AddCuts({vtx_chi2_track_cut, nhits_cut, chi2_cut});
+  return vertex_tracks_cuts;
 };
+
+AnalysisTree::Cuts* GetCbmCentralityTrackCuts() {
+  Cuts* vertex_tracks_cuts = new Cuts("VertexTracks");
+  SimpleCut vtx_chi2_track_cut("vtx_chi2", 0, 3);
+  SimpleCut nhits_cut("nhits", 4, 100);
+  SimpleCut chi2_cut({"chi2", "nhits"}, [](std::vector<double> par){ return par[0]/par[1] < 10; });
+  SimpleCut eta_cut("eta", 0, 3);
+
+  vertex_tracks_cuts->AddCuts({vtx_chi2_track_cut, nhits_cut, chi2_cut, eta_cut});
+  return vertex_tracks_cuts;
+};
+
+AnalysisTree::Cuts* GetCbmTofHitsCuts() {
+  Cuts* tof_cuts = new Cuts("TofHits");
+  SimpleCut tof_cuts_dx("dx", -2, 2);
+  SimpleCut tof_cuts_dy("dy", -1, 1);
+//  SimpleCut mc_cut("mc_pdg", 210, 212);
+
+  tof_cuts->AddCuts({tof_cuts_dx, tof_cuts_dy});
+  return tof_cuts;
+};
+
+AnalysisTree::Cuts* GetCbmMcTracksCuts() {
+
+  Cuts* sim_tracks_cut = new Cuts("sim_tracks_cut");
+  SimpleCut mother_id_cut("mother_id", -1000, -0.5);
+  sim_tracks_cut->AddCuts({mother_id_cut});
+
+  return sim_tracks_cut;
+};
+
+
+
+
+
