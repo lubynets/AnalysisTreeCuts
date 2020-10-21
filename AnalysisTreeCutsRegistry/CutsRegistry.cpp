7ebbcb5932942102fbe825fb928806c3312fded9
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2020-10-21 18:11:56 +0300

    Fix bug in GetLookupPath

--- AnalysisTreeCutsRegistry/CutsRegistry.cpp
+++ AnalysisTreeCutsRegistry/CutsRegistry.cpp
@@ -24,7 +24,7 @@ void AnalysisTree::RegisterCuts(const std::string& name, const Cuts &cuts) {
 std::vector<fs::path> GetLookupPaths() {
   std::vector<fs::path> result;
   result.emplace_back(fs::current_path());
-  if (std::string env_path_str{AnalysisTree::CutsEnvironNameForLibraryPath()};
+  if (std::string env_path_str{std::getenv(AnalysisTree::CutsEnvironNameForLibraryPath())};
       !env_path_str.empty()) {
     try {
       result.emplace_back(fs::path(env_path_str));
